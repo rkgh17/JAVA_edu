e8890e92cbaca71bd12a4ae286dc8de732e16e86
황지훈 <rkgh17@gmail.com>
2022-08-16 09:48:48 +0900

    morning

---
+++ work_space/JAVA_02/src/Exercise_/Member.java
@@ -0,0 +1,14 @@
+package Exercise_;
+
+public class Member {
+	public String name;
+	public String id;
+	public String password;
+	public int age;
+	
+	public Member (String name, String id) {
+		this.name = name;
+		this.id = id;
+		
+	}
+}
---
+++ work_space/JAVA_02/src/Exercise_/MemberServiceExample.java
@@ -0,0 +1,8 @@
+package Exercise_;
+
+public class MemberServiceExample {
+
+	public static void main(String[] args) {
+	}
+
+}
