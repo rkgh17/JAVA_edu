254f55ffd43c79e42f88006c1a3d846a3046af6c
황지훈 <rkgh17@gmail.com>
2022-08-17 11:16:32 +0900

    2t

---
+++ work_space/JAVA_02/src/exam_20_gettersetter/Car.java
@@ -0,0 +1,45 @@
+package exam_20_gettersetter;
+
+public class Car {
+	private int currentSpeed;
+	private int maxSpeed;
+
+	public Car(int currentSpeed, int maxSpeed) {
+		this.currentSpeed = currentSpeed;
+		this.maxSpeed = maxSpeed;
+	}
+
+	public int getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public void setCurrentSpeed(int currentSpeed) {
+		if (currentSpeed > this.maxSpeed)
+			this.currentSpeed = this.maxSpeed;
+		else
+			this.currentSpeed = currentSpeed;
+	}
+
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public void speedUp() {
+		this.currentSpeed++;
+	}
+
+//	public void setCurrentSpeed(int currentSpeed) {
+//		if (currentSpeed > this.maxSpeed)
+//			this.currentSpeed = this.maxSpeed;
+//		else
+//			this.currentSpeed = currentSpeed;
+//	}
+//	public int getCurrentSpeed() {
+//		return currentSpeed;
+//	}
+
+}
