1efa34cbd141202cc4e95c3d543989c4b7986ab7
황지훈 <rkgh17@gmail.com>
2022-08-22 15:42:17 +0900

    day10_6tt

---
+++ work_space/JAVA)05/src/exam02/Cat.java
@@ -0,0 +1,8 @@
+package exam02;
+
+public class Cat extends Animal{
+	public Cat() {
+		System.out.println("Cat 객체 생성");
+	}
+
+}
---
+++ work_space/JAVA)05/src/exam02/Dog.java
@@ -0,0 +1,7 @@
+package exam02;
+
+public class Dog extends Animal{
+	public Dog() {
+		System.out.println("Dog 객체 생성");
+	}
+}
