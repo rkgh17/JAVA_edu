395905c831d3b69b95d9cb5c3725bc1fce69ce40
황지훈 <rkgh17@gmail.com>
2022-08-23 14:33:49 +0900

    day11_5t

---
+++ work_space/JAVA_06/src/exam08/Student.java
@@ -0,0 +1,17 @@
+package exam08;
+
+import java.util.Objects;
+
+public class Student {
+	int sno;
+	String name;
+	Student(int sno, String name){
+		this.sno = sno;
+		this.name = name;
+	}
+	
+	public int hashCode() {
+		return Objects.hash(sno, name);
+	}
+
+}
