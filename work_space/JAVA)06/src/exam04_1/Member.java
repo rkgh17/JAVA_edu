cdbdd0c862637f46c73c657bc284d1ec89ad0478
황지훈 <rkgh17@gmail.com>
2022-08-23 11:52:28 +0900

    day11_3t

--- work_space/JAVA)06/src/exam04_1/Member.java
+++ work_space/JAVA)06/src/exam04_1/Member.java
@@ -6,13 +6,15 @@ public class Member implements Cloneable{
 	public String password;
 	public int age;
 	public boolean adult;
+	public int[] a;
 	
-	public Member(String id, String name, String password, int age, boolean adult) {
+	public Member(String id, String name, String password, int age, boolean adult, int[] a) {
 		this.id = id;
 		this.name = name;
 		this.password = password;
 		this.age = age;
 		this.adult = adult;
+		this.a = a;
 	}
 	
 	public Member getMember() {
