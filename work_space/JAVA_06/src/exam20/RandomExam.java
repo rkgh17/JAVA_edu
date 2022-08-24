135a8fc539545fc03029fe94b949278f7ca8da5a
황지훈 <rkgh17@gmail.com>
2022-08-24 14:31:36 +0900

    day12_5t

--- work_space/JAVA_06/src/exam20/RandomExam.java
+++ work_space/JAVA_06/src/exam20/RandomExam.java
@@ -6,24 +6,34 @@ import java.util.Random;
 public class RandomExam {
 
 	public static void main(String[] args) {
+		
+		long seed1;
+		seed1 = System.currentTimeMillis();
+		
+		
 		//선택 번호
 		int[] selectNumber = new int[6];
-		Random random = new Random(3);
+//		Random random = new Random(3);
+		Random random1 = new Random(seed1);
 		// 컴퓨터에서 난수를 발생시키는 기준인 seed라는 것이 있음.
 		// 3은 seed를 의미함
 		System.out.print("선택 번호: ");
 		for(int i = 0 ; i<selectNumber.length ; i++) {
-			selectNumber[i] = random.nextInt(45)+1;
-			// nextint는 1부터 45까지의 난수를 뽑는것
+			selectNumber[i] = random1.nextInt(45)+1;
+			// nextint는 1부터 45까지의 중복되지 않는 수를 뽑음
 			System.out.print(selectNumber[i] + " ");
 		}
 		System.out.println();
 		
+		long seed2;
+		seed2 = System.currentTimeMillis();
+		
 		//당첨번호
 		int[] winningNumber = new int[6];
-		random = new Random(5);
+		Random random2 = new Random(seed2);
+		// Random(3)으로 바꾸면 seed가 동일하므로 같은 수 나옴
 		for(int i = 0 ; i<winningNumber.length ; i++) {
-			winningNumber[i] = random.nextInt(45)+1;
+			winningNumber[i] = random2.nextInt(45)+1;
 			System.out.print(winningNumber[i] + " ");
 		}
 		System.out.println();
---
+++ work_space/JAVA_06/src/exam21/TimeZoneExam.java
@@ -0,0 +1,13 @@
+package exam21;
+
+import java.util.TimeZone;
+
+public class TimeZoneExam {
+	public static void main(String[] args) {
+		String[] availableIDs = TimeZone.getAvailableIDs(); // 세계시의 구분
+		for (String id : availableIDs) {
+			System.out.println(id);
+		}
+	}
+
+}
