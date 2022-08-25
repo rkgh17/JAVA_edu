391b7d2db9862074f572e0a6dd161f237323bf70
황지훈 <rkgh17@gmail.com>
2022-08-25 09:51:18 +0900

    day13_1t

--- work_space/JAVA_07/src/example3/ContainerExam.java
+++ work_space/JAVA_07/src/example3/ContainerExam.java
@@ -7,11 +7,16 @@ public class ContainerExam {
 		container1.set("홍길동", "도적");
 		String name1 = container1.getKey();
 		String job = container1.getValue();
+		System.out.println(name1);
+		System.out.println(job);
 
-		Container<String, String> container2 = new Container<String, String>();
+		Container<String, Integer> container2 = 
+				new Container<String, Integer>();
 		container2.set("홍길동", 35);
 		String name2 = container2.getKey();
 		int age = container2.getValue();
+		System.out.println(name2);
+		System.out.println(age);
 	}
 
 }
